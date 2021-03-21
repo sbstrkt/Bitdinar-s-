f71928fc3e764b770ceb33fbf2c9ea12dfd2d926
sbstrkt <os@v1nk.io>
2021-03-21 23:43:34 +0100

    ping-alpha

---
+++ bitdinars-source/src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitDinars-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitDinarsURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
